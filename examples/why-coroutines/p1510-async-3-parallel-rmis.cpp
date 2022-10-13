4f552dbf1c854aade344e8571ff495660896fa5e
JohanVanslembrouck <66462599+JohanVanslembrouck@users.noreply.github.com>
2022-10-13 18:38:38 +0200

    Simplified p1510-async-3-parallel-rmis.cpp

--- examples/why-coroutines/p1510-async-3-parallel-rmis.cpp
+++ examples/why-coroutines/p1510-async-3-parallel-rmis.cpp
@@ -24,50 +24,25 @@ public:
     {
         printf("Class01::function1()\n");
         remoteObj1.sendc_op1(gin11, gin12, 
-            [this](int out1, int out2, int ret1) { this->function1a(out1, out2, ret1); });
+            [this](int out1, int out2, int ret1) { this->function1a(0, out1, out2, ret1); });
         remoteObj2.sendc_op1(gin11, gin12, 
-            [this](int out1, int out2, int ret1) { this->function1b(out1, out2, ret1); });
+            [this](int out1, int out2, int ret1) { this->function1a(1,  out1, out2, ret1); });
         remoteObj3.sendc_op1(gin11, gin12, 
-            [this](int out1, int out2, int ret1) { this->function1c(out1, out2, ret1); });
+            [this](int out1, int out2, int ret1) { this->function1a(2, out1, out2, ret1); });
     }
 
-    void function1a(int out11, int out12, int ret1)
+    void function1a(int index, int out11, int out12, int ret1)
     {
         printf("Class01::function1a(%d, %d, %d)\n", out11, out12, ret1);
-        call1finished = true;
-        result1 = ret1;
-        function1d();
-    }
-    
-    void function1b(int out11, int out12, int ret1)
-    {
-        printf("Class01::function1b(%d, %d, %d)\n", out11, out12, ret1);
-        call2finished = true;
-        result2 = ret1;
-        function1d();
-    }
-    
-    void function1c(int out11, int out12, int ret1)
-    {
-        printf("Class01::function1c(%d, %d, %d)\n", out11, out12, ret1);
-        call3finished = true;
-        result3 = ret1;
-        function1d();
-    }
-
-    void function1d()
-    {
-		if (call1finished && call2finished && call3finished)
-			printf("Class01::function1d: result = %d\n", result1 + result2 + result3);
+        callfinished[index] = true;
+        result[index] = ret1;
+        if (callfinished[0] && callfinished[1] && callfinished[2])
+            printf("Class01::function1a: result = %d\n", result[0] + result[1] + result[2]);
     }
     
 private:
-    bool call1finished{false};
-    bool call2finished{false};
-    bool call3finished{false};
-    int result1{0};
-    int result2{0};
-    int result3{0};
+    bool callfinished[3]{ false, false, false };
+    int result[3]{ 0, 0, 0 };
 };
 
 Class01 class01;
